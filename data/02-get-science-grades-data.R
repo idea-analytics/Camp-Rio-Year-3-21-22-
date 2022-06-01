56c2f2b4c04aee437f6650044a2a90caa4701939
Steven Macapagal <sf.macapagal@gmail.com>
2022-06-01 12:41:27 -0500

    get data for act, science, gpa

---
+++ data/02-get-science-grades-data.R
@@ -0,0 +1,22 @@
+################################################################################
+# Filename: 02-get-science-grades-data.R                                       #
+# Path: ./data/02-get-science-grades-data.R                                    #
+# Author: Steven Macapagal                                                     #
+# Date created: 2022-06-01                                                     #
+# Date modified: 2022-06-01                                                    #
+# Purpose: This script pulls in science historical grades.                     #
+# Inputs:                                                                      #
+# Outputs:                                                                     #
+# Notes:                                                                       #
+################################################################################
+
+
+df_science_grades <- get_table(.table_name = "StudentHistoricalGrades",
+                               .server_name = "RGVPDSD-DWPRD1",
+                               .database_name = "PROD1",
+                               .schema = "Schools") %>%
+  filter(AcademicYear == "2021-2022",
+         CreditType == "SC")
+
+
+glimpse(df_science_grades)
---
+++ data/03-get-gpa-data.R
@@ -0,0 +1,18 @@
+################################################################################
+# Filename: 03-get-gpa-data.R                                                  #
+# Path: ./data/03-get-gpa-data.R                                               #
+# Author: Steven Macapagal                                                     #
+# Date created: 2022-06-01                                                     #
+# Date modified: 2022-06-01                                                    #
+# Purpose: This script pulls in science historical grades.                     #
+# Inputs:                                                                      #
+# Outputs:                                                                     #
+# Notes:                                                                       #
+################################################################################
+
+
+df_gpa <- get_table(.table_name = "StudentAcademicSummary",
+                    .server_name = "RGVPDSD-DWPRD1",
+                    .database_name = "PROD1",
+                    .schema = "Schools") %>%
+  filter(AcademicYear == "2021-2022")
