19d5bc4b4a73a4809b319b8e7d90859b4a52fd53
Steven Macapagal <sf.macapagal@gmail.com>
2022-06-02 15:04:00 -0500

    modify db sources
    1. import data from backup databases
    2. add excel file for act data

--- data/02-get-science-grades-data.R
+++ data/02-get-science-grades-data.R
@@ -3,7 +3,7 @@
 # Path: ./data/02-get-science-grades-data.R                                    #
 # Author: Steven Macapagal                                                     #
 # Date created: 2022-06-01                                                     #
-# Date modified: 2022-06-01                                                    #
+# Date modified: 2022-06-02                                                    #
 # Purpose: This script pulls in science historical grades.                     #
 # Inputs:                                                                      #
 # Outputs:                                                                     #
@@ -11,12 +11,22 @@
 ################################################################################
 
 
+# df_science_grades <- get_table(.table_name = "StudentHistoricalGrades",
+#                                .server_name = "RGVPDSD-DWPRD1",
+#                                .database_name = "PROD1",
+#                                .schema = "Schools") %>%
+#   filter(AcademicYear == "2021-2022",
+#          CreditType == "SC",
+#          StoreCode == "Y1")
+
+
 df_science_grades <- get_table(.table_name = "StudentHistoricalGrades",
-                               .server_name = "RGVPDSD-DWPRD1",
+                               .server_name = "1065574-SQLPRD1",
                                .database_name = "PROD1",
                                .schema = "Schools") %>%
   filter(AcademicYear == "2021-2022",
-         CreditType == "SC")
+         CreditType == "SC",
+         StoreCode == "Y1")
 
 
 glimpse(df_science_grades)
--- data/03-get-gpa-data.R
+++ data/03-get-gpa-data.R
@@ -3,7 +3,7 @@
 # Path: ./data/03-get-gpa-data.R                                               #
 # Author: Steven Macapagal                                                     #
 # Date created: 2022-06-01                                                     #
-# Date modified: 2022-06-01                                                    #
+# Date modified: 2022-06-02                                                    #
 # Purpose: This script pulls in science historical grades.                     #
 # Inputs:                                                                      #
 # Outputs:                                                                     #
@@ -11,8 +11,16 @@
 ################################################################################
 
 
+# df_gpa <- get_table(.table_name = "StudentAcademicSummary",
+#                     .server_name = "RGVPDSD-DWPRD1",
+#                     .database_name = "PROD1",
+#                     .schema = "Schools") %>%
+#   filter(AcademicYear == "2021-2022")
+
+
 df_gpa <- get_table(.table_name = "StudentAcademicSummary",
-                    .server_name = "RGVPDSD-DWPRD1",
+                    .server_name = "1065574-SQLPRD1",
                     .database_name = "PROD1",
                     .schema = "Schools") %>%
-  filter(AcademicYear == "2021-2022")
+  filter(AcademicYear == "2021-2022") %>%
+  collect()
